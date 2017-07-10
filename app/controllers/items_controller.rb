29359423ccbca4f9537bae03311220cd6ece262e
Eon <eoneon123@gmail.com>
2017-07-10 12:17:51 -0700

    added items#controller and views -> next: list properties fields on form which requires incorporating item_types

---
+++ app/controllers/items_controller.rb
@@ -0,0 +1,68 @@
+class ItemsController < ApplicationController
+  def index
+    @items = Item.all
+  end
+
+  def show
+    @item = Item.find(params[:id])
+  end
+
+  def new
+    @item = Item.new
+  end
+
+  def create
+    @item = Item.new(item_params)
+
+    if @item.save
+      flash[:notice] = "Item was saved successfully."
+      redirect_to @item
+    else
+      flash.now[:alert] = "Error creating item. Please try again."
+      render :new
+    end
+  end
+
+  def edit
+    @item = Item.find(params[:id])
+  end
+
+  def update
+    @item = Item.find(params[:id])
+    @item.assign_attributes(item_params)
+
+    if @item.save
+      flash[:notice] = "Item was updated successfully."
+      redirect_to @item
+    else
+      flash.now[:alert] = "Error updated item. Please try again."
+      render :edit
+    end
+  end
+
+  def destroy
+    @item = Item.find(params[:id])
+
+    if @item.destroy
+      flash[:notice] = "\"#{@item.sku}\" was deleted successfully."
+      redirect_to action: :index
+    else
+      flash.now[:alert] = "There was an error deleting the item."
+      render :show
+    end
+  end
+
+  private
+
+  def item_params
+    #unpermitted params = :name, :leg_count
+    # params.require(:item).permit(:sku, :item_type_id, :properties => {})
+    #syntax error, unexpected ',', expecting => ...type_id, :properties => {:name, :leg_count}) ... ^
+    #params.require(:item).permit(:sku, :item_type_id, :properties => {:name, :leg_count})
+    #https://stackoverflow.com/questions/19172893/rails-hashes-with-unknown-keys-and-strong-parameters
+    properties = params[:item].delete(:properties)
+    params.require(:item).permit(:name).tap do |whitelisted|
+       whitelisted[:properties] = properties
+     end
+  end
+end
