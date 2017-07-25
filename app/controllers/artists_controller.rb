6a97774381dd3f1b69d55b60f8dc79c7503b3db5
Eon <eoneon123@gmail.com>
2017-07-24 20:38:47 -0700

    added artists actions and views

--- app/controllers/artists_controller.rb
+++ app/controllers/artists_controller.rb
@@ -1,2 +1,60 @@
 class ArtistsController < ApplicationController
+  def index
+    @artists = Artist.all
+  end
+
+  def show
+    @artist = Artist.find(params[:id])
+  end
+
+  def new
+    @artist = Artist.new
+  end
+
+  def create
+    @artist = Artist.new(artist_params)
+
+    if @artist.save
+      flash[:notice] = "Artist was saved successfully."
+      redirect_to @artist
+    else
+      flash.now[:alert] = "Error creating Artist. Please try again."
+      render :new
+    end
+  end
+
+  def edit
+    @artist = Artist.find(params[:id])
+  end
+
+  def update
+    @artist = Artist.find(params[:id])
+    @artist.assign_attributes(artist_params)
+
+    if @artist.save
+      flash[:notice] = "Artist was updated successfully."
+      redirect_to @artist
+    else
+      flash.now[:alert] = "Error updated Artist. Please try again."
+      render :edit
+    end
+  end
+
+  def destroy
+    @artist = Artist.find(params[:id])
+
+    if @artist.destroy
+      flash[:notice] = "\"#{@artist.sku}\" was deleted successfully."
+      redirect_to action: :index
+    else
+      flash.now[:alert] = "There was an error deleting the Artist."
+      render :show
+    end
+  end
+
+  private
+
+  def artist_params
+    params.require(:artist).permit(:first_name, :last_name)
+  end
 end
