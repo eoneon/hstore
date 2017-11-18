b8509177bdddc713afed99e3daa6714408d409ef
Eon <eoneon123@gmail.com>
2017-11-18 08:20:41 -0800

    extended js add field to artst -> display

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,10 +1,10 @@
 module ApplicationHelper
   #see rails cast 196
-  def link_to_add_fields(name, f, association)
+  def link_to_add_fields(name, f, association, parent)
     new_object = f.object.send(association).klass.new
     id = new_object.object_id
     fields = f.fields_for(association, new_object, child_index: id) do |builder|
-      render("item_types/" + association.to_s.singularize + "_fields", f: builder)
+      render("#{parent}/" + association.to_s.singularize + "_fields", f: builder)
     end
     link_to(name, '#', class: "add_fields", data: {id: id, fields: fields.gsub("\n", "")})
   end
