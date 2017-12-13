8df24f12eec25eff2a4528edcff8fd3e663badcc
Eon <eoneon123@gmail.com>
2017-12-13 09:58:28 -0800

    updated cert type to handle seals

--- app/helpers/certificate_types_helper.rb
+++ app/helpers/certificate_types_helper.rb
@@ -6,20 +6,26 @@ module CertificateTypesHelper
   def cert_psa_dna(item)
     if item.properties["certificate_kind"].present?
       if item.properties["certificate_kind"] == "PSA/DNA authentication"
-        ["PSA/DNA Authentication", "This piece is presented with PSA/DNA Authentication, which authenticates memorabilia using proprietary permanent invisible ink as well as a strand of synthetic DNA."]
+        ["with PSA/DNA Authentication", "This piece is presented with PSA/DNA Authentication, which authenticates memorabilia using proprietary permanent invisible ink as well as a strand of synthetic DNA."]
       # else
       #   ""
       end
     end
   end
 
+  def seal(item)
+    if item.properties["seal_kind"].present? && item.properties["seal_issuer"].present?
+      #{}"#{["This piece bears the", item.properties["seal_kind"].split(" ")[0], item.properties["seal_issuer"], item.properties["seal_kind"].split(" ")[1]].join(" ")}."
+      "This piece bears the #{item.properties["seal_kind"]} from #{item.properties["seal_issuer"]}."
+    end
+  end
+
   def certificate(item)
     if item.properties["certificate_kind"].present?
       if item.properties["certificate_kind"] != "PSA/DNA authentication"
-        # cert = [ item.properties["certificate_kind"], item.properties["certificate_issuer"] ].reject { |item| item.blank? }.join(" from ")
-        # ["with #{cert}", "Includes #{cert}."]
         cert = [ item.properties["certificate_kind"], item.properties["certificate_issuer"] ].reject { |item| item.blank? }.join(" from ")
-        ["with #{cert}", "Includes #{cert}."]
+        intro = item.properties["certificate_kind"] == "seal of authenticity" ? ["bearing", "This piece bears the"] : ["with", "Includes"]
+        ["#{intro[0]} #{cert}", "#{intro[-1]} #{cert}."]
       # else
       #   [""]
       end
@@ -31,7 +37,7 @@ module CertificateTypesHelper
       #["with #{certificate(item)}", "Includes #{certificate(item)}."]
       #[ [ certificate(item)[0], certificate(item)[-1] ], [ cert_psa_dna(item).try([0]), cert_psa_dna(item).try([-1]) ] ].reject { |item| item.blank? }
       cert = [ certificate(item), cert_psa_dna(item) ].reject { |item| item.blank? }[0]
-      [cert[0], cert[-1]]
+      [ cert[0], cert[1] ]
     else
       [""]
     end
